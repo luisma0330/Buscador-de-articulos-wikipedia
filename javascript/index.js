3c8be4e5dfd5e483bc23ac71c3986b4c972b9a7b
luisma0330 <luisma0330@hotmail.com>
2021-08-30 02:01:37 -0500

    correccion bug url

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ function search(s){
     var ids = "";
     var links = [];
     var results = [];
-    fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='+s)
+    fetch('https://es.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='+s)
     .then(response => {
         return response.json();
     })
@@ -10,27 +10,16 @@ function search(s){
         results = result.query.search;
         for(var i=0; i<results.length; i++){
             if(results[i+1] != null){
-                ids += results[i].pageid+"|";
+                ids = ids + results[i].pageid + "|";
             }else{
-                ids+=results[i].pageid;
+                ids = ids + results[i].pageid;
             }
         }   
     })
-    .then(a => {
-    fetch("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids="+ids)
-    .then(idresult => {
-        return idresult.json();
-        })
-        .then(idresult =>{
-            for(i in idresult.query.pages){
-                links.push(idresult.query.pages[i].fullurl);
-            }
-        })
-        .then(g =>{
-            document.getElementById("output").innerHTML="";
-            for(var i=0; i<results.length; i++){
-                document.getElementById("output").innerHTML+="<a href='"+links[i]+"' target='_blank'>"+results[i].title+"</a><br>"+results[i].snippet+"<br>";
-            }
-        });
+    .then(g => {
+        document.getElementById("output").innerHTML="";
+        for(var i=0; i<results.length; i++){
+                document.getElementById("output").innerHTML+="<a href='https://es.wikipedia.org/?curid="+results[i].pageid+"' target='_blank'>"+results[i].title+"</a><br>"+results[i].snippet+"<br>";
+        }
     });
 }
