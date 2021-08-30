93b492c9b050e9a4896aa3725630710efb2366eb
luisma0330 <luisma0330@hotmail.com>
2021-08-30 00:33:13 -0500

    correccion de funcionamiento api

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,32 @@
 function search(s){
     var ids = "";
     var links = [];
-    var results =[];
-    fetch('https://es.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=' + s)
+    var results = [];
+    fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='+s)
     .then(response => {
         return response.json();
     })
-    .then(result =>{
+    .then(result => {
         results = result.query.search;
         for(var i=0; i<results.length; i++){
             if(results[i+1] != null){
-                ids += results[i].pageid + "|";
+                ids += results[i].pageid+"|";
             }else{
-                ids += results[i].pageid;
+                ids+=results[i].pageid;
             }
-        }
+        }   
     })
     .then(a => {
-        fetch("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids=" + ids)
-        .then(idresult => {
-            return idresult.json();
+    fetch("https://en.wikipedia.org/w/api.php?action=query&prop=info&inprop=url&origin=*&format=json&pageids="+ids)
+    .then(idresult => {
+        return idresult.json();
         })
-        .then(idresult => {
+        .then(idresult =>{
             for(i in idresult.query.pages){
                 links.push(idresult.query.pages[i].fullurl);
             }
         })
-        .then(g => {
+        .then(g =>{
             document.getElementById("output").innerHTML="";
             for(var i=0; i<results.length; i++){
                 document.getElementById("output").innerHTML+="<a href='"+links[i]+"' target='_blank'>"+results[i].title+"</a><br>"+results[i].snippet+"<br>";
